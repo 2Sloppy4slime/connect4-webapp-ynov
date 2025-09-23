01c4c9ed4849c50f87773d5b3d28469dd0c47d74
Lowaax <florin.hamelin@gmail.com>
2025-09-23 10:48:15 +0200

    Merge branch 'main' of https://github.com/2Sloppy4slime/ynov_power4-web

--- webapp.go
+++ webapp.go
@@ -3,13 +3,23 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 type PageData struct {
 	Message string
 }
 
+var a [7][6]int
+var turn bool //false = red, true = yellow
+
 func main() {
+	turn = false
+	for _, val := range a {
+		for i := range val {
+			val[i] = 0
+		}
+	}
 	fs := http.FileServer(http.Dir("css"))
 	http.Handle("/css/", http.StripPrefix("/css/", fs))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +28,7 @@ func main() {
 
 	})
 	http.HandleFunc("/submit", submit)
+	http.HandleFunc("/turn", DoTurn)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -31,4 +42,30 @@ func submit(w http.ResponseWriter, r *http.Request) {
 
 	}
 
+}
+func DoTurn(w http.ResponseWriter, r *http.Request) {
+	x, err := strconv.Atoi(r.FormValue("x"))
+	if err == nil {
+		placepiece(x)
+		return
+	}
+	print("error ntm touche pas a mon code connard de tes morts")
+
+}
+func placepiece(x int) {
+	for i := 0; i <= 7; i++ {
+
+		if a[x][i] == 0 {
+			if turn {
+				a[x][i] = 1
+				turn = !turn
+				return
+			} else {
+				a[x][i] = 2
+				turn = !turn
+				return
+			}
+		}
+	}
+	//pas pu placer le pion :(
 }
